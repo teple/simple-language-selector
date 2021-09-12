5f2ca1f89bbb1e8ec7c205ba15a5e079e723049d
byeongseon-kang <b_kang@codmon.co.jp>
2021-09-12 16:49:44 +0900

    fix: scss 를 사용할 수 있도록 패키지 설치와 webpack 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 
 /**
  * @type import("webpack").Configuration
@@ -9,7 +11,8 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   devtool: 'inline-source-map',
   entry: {
-    main: `${__dirname}/src/main.ts`
+    main: `${__dirname}/src/main.ts`,
+    style: `${__dirname}/src/style/style.scss`
   },
   module: {
     rules: [
@@ -17,11 +20,22 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/
       }
     ]
   },
   resolve: {
     extensions: ['.ts', '.js']
   },
-  plugins: [new CopyPlugin([{ from: './public', to: './' }])]
+  plugins: [
+    new CopyPlugin([{ from: './public', to: './' }]),
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
 }
